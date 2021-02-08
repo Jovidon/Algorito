9174b320a252a398d5361f1344896fdac5c11159
Jovid Xamrokulov <jxamrokulov333@gmail.com>
2021-02-08 13:24:38 +0500

    Linear and  Binary search algorithms implemented.

---
+++ Algorithms/Searching/LinearSearch.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+/*
+*
+*  Implementation of Linear Search in C++
+*  O(n) Time Complexity
+*
+*/
+
+int linearSearch(int arr[], int size, int key) {
+	
+	for(int i=0; i<size; i++) {
+		if(arr[i] == key) return i;
+	}
+	
+	return -1;
+}
+
+
+int main () {
+	
+	int arr[] = { 0, 13, 24, 333, 401 }; 
+    int key = 334; 
+    int size = sizeof(arr) / sizeof(arr[0]);
+    
+    int found = linearSearch(arr, size, key);
+    
+    (found == -1) 
+        ? cout << "Item is not present in array"
+        : cout << "Item is present at index " << found; 
+    
+    
+}
